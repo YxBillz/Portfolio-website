63f456a361c8ac5e0cbd2b1ff82b0137cc842591
Stephen <ad1859@student.jamk.fi>
2024-11-30 20:37:06 +0200

    Added app description, image and links

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])
 
 with col1:
     st.image("image.jpg")
@@ -28,8 +28,13 @@ df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source Code](https://.com)")
 
 with col4:
-    for index, row in df[:10].iterrows():
+    for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[Source Code](https://.com)")
