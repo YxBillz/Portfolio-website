3d9b66f05f8bf262110bdf2bece6e768b6006e90
Stephen <ad1859@student.jamk.fi>
2024-11-30 20:12:25 +0200

    Rendered the app titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -10,7 +11,25 @@ with col1:
 with col2:
     st.title("Obiri-Ibe Stephen")
     content = """
-    Hello! I'm a passionate Business Information Technology student currently diving deep into the world of Python programming. My academic background has provided me with a good foundation in technology and business integration, and I am now equipping myself with the skills to thrive in the evolving tech industry.
+    Hello! I'm a passionate Business Information Technology student at JAMK University of Applied Sciences Jyväskylä and I'm currently diving deep into the world of Python programming. My academic background has provided me with a good foundation in technology and business integration, and I am now equipping myself with the skills to thrive in the evolving tech industry.
 I’m currently focused on mastering Python, where I’m exploring its applications in data analysis, automation, and web development. My enthusiasm for learning and problem-solving drives me to create innovative solutions that bridge the gap between technology and real-world challenges.
     """
     st.info(content)
+
+content2 = """
+You can find some of the apps I have built in python below. Please, feel free to contact me!
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+
+with col4:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
